d2ac94862b520252e2cf55d6caa8bde6930c73c3
yan358941877 <yan358941877@163.com>
2017-06-26 21:48:10 +0800

    add: 增加RouterMap中相关的几个page

---
+++ src/lol/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <h1>This is HomePage Component</h1>
+  )
+}
---
+++ src/lol/pages/SearchPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SearchPage() {
+  return (
+    <h1>This is SearchPage Component</h1>
+  )
+}
