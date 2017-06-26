0b2749ee2ea492b41f79c500eb1705a9ce3394e5
yan358941877 <yan358941877@163.com>
2017-06-26 21:36:50 +0800

    add: configue store

---
+++ src/lol/store/enhancer.js
@@ -0,0 +1,6 @@
+import { applyMiddleware } from 'redux'
+import reduxPromise from 'redux-promise'
+
+import progressMiddleware from '../middleware/ProgressMiddleware'
+
+export default applyMiddleware(progressMiddleware, reduxPromise)
