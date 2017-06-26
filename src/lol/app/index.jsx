ac6556ca11096353482182e65af7b0e717fbfee6
yan358941877 <yan358941877@163.com>
2017-06-26 23:09:57 +0800

    modify: pure function式组件的改写

--- src/lol/app/index.jsx
+++ src/lol/app/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-class App extends React.Component {
-  static propTypes = {
-    children: propTypes.element.isRequired
-  }
+function App(props) {
+  return (
+    <div>
+      <h1>This is App</h1>
+      {props.children}
+    </div>
+  )
+}
 
-  render() {
-    return (
-      <div>
-        <h1>This is App</h1>
-        {this.props.children}
-      </div>
-    )
-  }
+App.propTypes = {
+  children: propTypes.element.isRequired
 }
 
 export default App
+// class App extends React.Component {
+//   static propTypes = {
+//     children: propTypes.element.isRequired
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is App</h1>
+//         {this.props.children}
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
