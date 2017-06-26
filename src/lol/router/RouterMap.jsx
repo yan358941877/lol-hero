775c562a968a287644b9fea07071a21ab85333f6
yan358941877 <yan358941877@163.com>
2017-06-26 21:46:49 +0800

    add: RouterMap

---
+++ src/lol/router/RouterMap.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { browserHistory, Router, IndexRoute, Route } from 'react-router'
+
+import App from '../app'
+import HomePage from '../pages/HomePage'
+import SearchPage from '../pages/SearchPage'
+
+export default function RouterMao() {
+  return (
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={HomePage} />
+        <Route path="/search" component={SearchPage} />
+      </Route>
+    </Router>
+  )
+}
