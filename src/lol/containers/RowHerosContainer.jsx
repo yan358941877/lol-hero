1cf4a18afe5eff389f23e45e8e0162c6eaa4a48b
yan358941877 <yan358941877@163.com>
2017-06-29 14:49:19 +0800

    modify: RowHerosContainer

--- src/lol/containers/RowHerosContainer.jsx
+++ src/lol/containers/RowHerosContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import Immutable from 'immutable'
 import HerosCovers from '../components/HerosCovers'
 import actionCreators from '../actions/hero-action-creators'
 
