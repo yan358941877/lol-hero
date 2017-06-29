edf855407843dc7c0dd1d308e4089f0a37fb4e96
yan358941877 <yan358941877@163.com>
2017-06-29 17:44:12 +0800

    add: 函数防抖与函数节流

--- src/lol/components/AppHeader.jsx
+++ src/lol/components/AppHeader.jsx
@@ -9,17 +9,63 @@ class AppHeader extends React.PureComponent {
     this.state = {
       translucent: true
     }
+    this.checkScroll = this.checkScroll.bind(this)
   }
   componentDidMount() {
-    $(document).on('scroll', () => {
-      const translucent = document.body.scrollTop > 5
-      if (this.state.translucent !== translucent) {
-        this.setState({
-          translucent
-        })
-      }
+    // $(document).on('scroll', () => {
+    //   console.log('aa')
+    //   const translucent = document.body.scrollTop > 5
+    //   if (this.state.translucent !== translucent) {
+    //     this.setState({
+    //       translucent
+    //     })
+    //   }
+    // })
+    $(document).on('scroll', this.throttle(this.checkScroll, 50))
+  }
+  checkScroll() {
+    console.log('aaa')
+    const translucent = document.body.scrollTop > 25
+    if (this.state.translucent !== translucent) {
+      this.setState({
+        translucent
+      })
+    }
+  }
+  debounce(fn, delay) {
+    let timer = null
+    return function () {
+      const context = this
+      const args = arguments
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        fn.apply(context, args)
+      }, delay)
+    }
+  }
+  throttle(fn, threshold) {
+    let last
+    let timer
+    threshold = !threshold ? 250 : threshold
 
-    })
+    return function () {
+      const context = this
+      const args = arguments
+
+      const now = +new Date()
+
+      if (last && now < last + threshold) {
+        clearTimeout(timer)
+
+        timer = setTimeout(() => {
+          last = now
+          fn.apply(context, args)
+        }, threshold)
+      } else {
+        last = now
+        fn.apply(context, args)
+      }
+    }
   }
   render() {
     return (
