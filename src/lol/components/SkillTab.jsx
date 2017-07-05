5fc8c1c47d279ea7fd0a543ba64d87214a167cd1
yan358941877 <yan358941877@163.com>
2017-07-05 16:07:14 +0800

    修改SkillTab为函数式组件

--- src/lol/components/SkillTab.jsx
+++ src/lol/components/SkillTab.jsx
@@ -2,21 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import '../res/SkillTab.less'
-class SkillTab extends React.Component {
-  static propTypes = {
-    skill: PropTypes.object.isRequired
-  }
-  render() {
-    console.log(this.props.skill)
-    const { skill } = this.props
-    return (
-      <div className="skill-tab">
-        <h3>{skill.name}</h3>
-        <p dangerouslySetInnerHTML={{ __html: skill.description }}/>
-        <p dangerouslySetInnerHTML={{ __html: skill.tooltip }} />
-      </div>
-    )
-  }
-}
 
-export default SkillTab
+
+//class SkillTab extends React.Component {
+//   static propTypes = {
+//     skill: PropTypes.object.isRequired
+//   }
+//   render() {
+//     console.log(this.props.skill)
+//     const { skill } = this.props
+//     return (
+//       <div className="skill-tab">
+//         <h3>{skill.name}</h3>
+//         <p dangerouslySetInnerHTML={{ __html: skill.description }} />
+//         <p dangerouslySetInnerHTML={{ __html: skill.tooltip }} />
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default SkillTab
+
+export default function SkillTab({ skill }) {
+  return (
+    <div className="skill-tab">
+      <h3>{skill.name}</h3>
+      <p dangerouslySetInnerHTML={{ __html: skill.description }} />
+      <p dangerouslySetInnerHTML={{ __html: skill.tooltip }} />
+    </div>
+  )
+}
+SkillTab.propTypes = {
+  skill: PropTypes.object.isRequired
+}
