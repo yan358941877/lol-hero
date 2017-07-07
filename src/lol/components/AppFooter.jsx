af974cd0c759626b9f9fb3e879a84e70a2b14468
yan358941877 <yan358941877@163.com>
2017-07-07 11:48:25 +0800

    自适应布局 打包

--- src/lol/components/AppFooter.jsx
+++ src/lol/components/AppFooter.jsx
@@ -6,7 +6,7 @@ export default function AppFooter() {
   return (
     <div className="app-footer">
       <div className="float-left copyright">&copy;2017-2018 Designed and Implemented by Yan Xin</div>
-      <a className="float-right github">Star for me</a>
+      <a className="float-right github" href="https://github.com/yan358941877/lol-hero" rel="noopener noreferrer" target="_blank">Star for me</a>
     </div>
   )
 }
